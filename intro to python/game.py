1f7134f42cb74b6f39267d62f56b8773a69422cd
Stephen Burns <sab_paddy@hotmail.com>
2020-06-10 17:37:56 +0100

    fixed all errors with the damn pep8

--- intro to python/game.py
+++ intro to python/game.py
@@ -1,56 +1,69 @@
 import time
 import random
 
+
 # function for talking to the user.
 def talkToPlayer(textToDisplay):
     print(textToDisplay)
-    # time.sleep(2)
+    time.sleep(2)
+
 
 # function to display the intro to game
 def intro(weapon, randomEnemy):
-    talkToPlayer("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-    talkToPlayer("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.")
+    talkToPlayer("You find yourself standing in an open field,"
+                 " filled with grass and yellow wildflowers.")
+    talkToPlayer("Rumor has it that a" + randomEnemy +
+            	 " is somewhere around here, and has"
+                 " been terrifying the nearby village.")
     talkToPlayer("Enter 1 to knock on the door of the house.")
     talkToPlayer("Enter 2 to peer into the cave.")
     talkToPlayer("What would you like to do?")
 
-# house options  
+
+# house options
 def house(weapon, randomEnemy):
     talkToPlayer("You approach the door of the house.")
-    talkToPlayer("You are about to knock when the door opens and out steps a "+ randomEnemy+ ".")
-    talkToPlayer("Eep! This is the "+ randomEnemy+ "'s house!")
-    talkToPlayer("The "+ randomEnemy + " attacks you!")
+    talkToPlayer("You are about to knock when the door opens " +
+                 "and out steps a " + randomEnemy + ".")
+    talkToPlayer("Eep! This is the " + randomEnemy + "'s house!")
+    talkToPlayer("The " + randomEnemy + " attacks you!")
     if "sword" not in weapon:
-        talkToPlayer("You feel a bit under-prepared for this, what with only having a tiny dagger.")
-    
+        talkToPlayer("You feel a bit under-prepared for" +
+                     " this, what with only having a tiny dagger.")
+
     while True:
         choice2 = input("Would you like to (1) fight or (2) run away?")
         if choice2 == "1":
             fight(weapon, randomEnemy)
             play_again()
-            break 
-        
+            break
+
         if choice2 == "2":
-            talkToPlayer("You run back into the field. Luckily, you don't seem to have been followed.")
+            talkToPlayer("You run back into the field. Luckily," +
+                         " you don't seem to have been followed.")
             field(weapon, randomEnemy)
             break
+
 
 def cave(weapon, randomEnemy):
     if "sword" in weapon:
         talkToPlayer("You peer cautiously into the cave.")
-        talkToPlayer("You\'ve been here before, and gotten all the good stuff. It\'s just an empty cave now.")
+        talkToPlayer("You\'ve been here before, and gotten all" +
+                     " the good stuff. It\'s just an empty cave now.")
         talkToPlayer("You walk back to the field.")
     else:
         talkToPlayer("You peer cautiously into the cave.")
         talkToPlayer("It turns out to be only a very small cave.")
         talkToPlayer("Your eye catches a glint of metal behind a rock.")
-        talkToPlayer("You have found the magical Sword of Ogoroth!")
-        talkToPlayer("You discard your silly old dagger and take the sword with you.")
+        talkToPlayer("You have found the magical" +
+                     " Sword of Ogoroth!")
+        talkToPlayer("You discard your silly old " +
+                     "dagger and take the sword with you.")
         talkToPlayer("You walk back out to the field.")
-        print(weapon)
         weapon.append("sword")
-    print(weapon)
+        # originally had weapon += "sword" .. used print to debug it XD
     field(weapon, randomEnemy)
+
 
 def field(weapon, randomEnemy):
     talkToPlayer("Enter 1 to knock on the door of the house.")
@@ -65,16 +78,23 @@ def field(weapon, randomEnemy):
             cave(weapon, randomEnemy)
             break
 
+
 def fight(weapon, randomEnemy):
     if "sword" in weapon:
-        talkToPlayer("As the "+ randomEnemy+ " moves to attack, you unsheath your new sword.")
-        talkToPlayer("The Sword of Ogoroth shines brightly in your hand as you brace yourself for the attack.")
-        talkToPlayer("But the " + randomEnemy + "takes one look at your shiny new toy and runs away!")
-        talkToPlayer("You have rid the town of the " + randomEnemy + ". You are victorious!")
+        talkToPlayer("As the " + randomEnemy +
+                     " moves to attack, you unsheath your new sword.")
+        talkToPlayer("The Sword of Ogoroth shines brightly" +
+                     " in your hand as you brace yourself for the attack.")
+        talkToPlayer("But the " + randomEnemy +
+                     "takes one look at your shiny new toy and runs away!")
+        talkToPlayer("You have rid the town of the " + randomEnemy +
+                     ". You are victorious!")
     else:
         talkToPlayer("You do your best...")
-        talkToPlayer("but your dagger is no match for the " + randomEnemy + ".")
+        talkToPlayer("but your dagger is no match for the " +
+                     randomEnemy + ".")
         talkToPlayer("You have been defeated!")
+
 
 def play_again():
     again = input("Would you like to play again? (y/n)").lower()
@@ -86,11 +106,11 @@ def play_again():
     else:
         play_again()
 
+
 def play_game():
     weapon = []
     enemies = ["Troll", "Dragon", "Pirate", "Ork", "Gargoyle"]
-    randomEnemy:str = random.choice(enemies)
-    print(randomEnemy)
+    randomEnemy = random.choice(enemies)
     intro(weapon, randomEnemy)
     field(weapon, randomEnemy)
 
