ee1175ffcaccdf75f6ad3c0bb00fcc8f45b41cd1
Stephen Burns <sab_paddy@hotmail.com>
2020-06-11 15:03:57 +0100

    2 d arrays and objects n stuff

--- intro to JavaScript/playground.js
+++ intro to JavaScript/playground.js
@@ -25,3 +25,38 @@ function returnDiv(){
     return newDiv;
 }
 
+// log some shiz
+var x = returnDiv();
+console.log("What have i done: ", x);
+
+// return a tuple 
+function color(r,g,b) {
+    var color = {red: r, green: g, blue: b}
+    return color;
+}
+
+// a map.. is an array that goes on a journey..
+// we take in an array.. transmute it.. and the result comes out..
+// an array of peoples energie levels.
+var arr = [100,75,75,100];
+// lets say these people excersize.. their energy will go down right?
+var exercizeArr = arr.map(function (energy){
+    energy = energy-20;
+    return energy;
+});
+// the values will now be reduced by 20.. as we excersized a bit.
+console.log(exercizeArr);
+
+
+// 2 dimetional arrays
+var twoArr =[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+// js object
+var person= {
+ name: "john",
+ age: 25,
+}
